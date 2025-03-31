652b6743d34d031be220b21df7f0e9acd93ffb79
mitan11 <mitantank00@gmail.com>
2025-03-31 16:53:53 +0530

    Refactor portfolio layout and components; remove unused files and update configurations

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Grid from '@/components/Grid'
+import Hero from '@/components/Hero'
+import RecentProjects from '@/components/RecentProjects'
+import { FloatingNav } from '@/components/ui/floating-navbar'
+import { navItems } from '@/data'
+import React from 'react'
+
+function page() {
+  return (
+    <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10'>
+      <div className='max-w-7xl w-full'>
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+      </div>
+    </main>
+  )
+}
+
+export default page
