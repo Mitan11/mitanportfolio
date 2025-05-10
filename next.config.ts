efe2c9143e20eaa84e47fa2c00224ee1c7ef27b2
mitan11 <mitantank00@gmail.com>
2025-05-10 19:04:58 +0530

    Bug Fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // devIndicators: false
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint:{
+    ignoreDuringBuilds: true
+  }
 };
 
 export default nextConfig;
